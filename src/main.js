9bcacffcaf0370e2a506fe8a088397a50a7aa23c
Last Whisper <xxzworkmail@gmail.com>
2020-10-30 19:15:23 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -6,11 +6,23 @@ import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/CSS/global.css'
+// 导入表格树
 import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 // 配置请求的根路径
+// 本机地址
+// axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 远程后台地址
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+
 // axios请求拦截
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
@@ -19,9 +31,28 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+// 组件全局注册 表格树
 Vue.component('tree-table', TreeTable)
+// 全局注册富文本编辑器
+Vue.use(VueQuillEditor)
+
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  // yyyy-mm-dd hh:mm:ss
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
